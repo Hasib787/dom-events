ffef8523803cf2fb8abdaf40c5579e2883033bd7
Hasib787 <mr.hasib45@gmail.com>
2021-01-26 02:39:20 +0600

    some function are added

--- js/function.js
+++ js/function.js
@@ -1,19 +1,44 @@
-function evenify(num) {
-    if (num % 2 === 0) {
-        console.log(num, ': is even number');
-    } else {
-        console.log(num * 2, ': is odd number');
-    }
-}
-function evenify_all(numbers){
+function evenify_all(numbers) {
     for (let i = 0; i < numbers.length; i++) {
         const num = numbers[i];
-        evenify(num);
+        if (num % 2 === 0) {
+            console.log(num, ': is even number');
+        } else {
+            console.log(num * 2, ': is odd number');
+        }
     }
 }
 
 let numbers = [5, 58, 46, 21, 55, 89, 20];
-    evenify_all(numbers);
+evenify_all(numbers);
 
 let friendsAge = [17, 25, 20, 14, 10, 24];
-    evenify_all(friendsAge);
+evenify_all(friendsAge);
+
+//using return
+function evenify(num) {
+    var result;
+    if (num % 2 == 0) {
+        result = num;
+    }
+    else {
+        result = num * 2;
+    }
+    return result;
+}
+let result=evenify(13);
+let square=result*result;
+console.log('square',square);
+
+function even_all(nums) {
+    let even_array = [];
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        let result = evenify(num);
+        even_array.push(result);
+    }
+    return even_array;
+}
+nums = [5, 9, 5, 8, 7, 88];
+let nums_even = even_all(nums);
+console.log(nums_even);
