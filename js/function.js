41ec836c5e69abb86d896548a1bbded4c2b37ace
Hasib787 <mr.hasib45@gmail.com>
2021-01-26 00:30:41 +0600

    adding some function

--- js/function.js
+++ js/function.js
@@ -1,9 +1,19 @@
-let numbers = [5, 58, 46, 21, 55, 89, 20];
-for (let i = 0; i < numbers.length; i++) {
-    const num = numbers[i];
+function evenify(num) {
     if (num % 2 === 0) {
         console.log(num, ': is even number');
     } else {
         console.log(num * 2, ': is odd number');
     }
 }
+function evenify_all(numbers){
+    for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i];
+        evenify(num);
+    }
+}
+
+let numbers = [5, 58, 46, 21, 55, 89, 20];
+    evenify_all(numbers);
+
+let friendsAge = [17, 25, 20, 14, 10, 24];
+    evenify_all(friendsAge);
