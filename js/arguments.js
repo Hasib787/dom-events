e3b3320229da59b3562dc69f8bd44e0b02f567e7
Hasib787 <mr.hasib45@gmail.com>
2021-01-31 16:18:43 +0600

    added a function with in arguments file

--- js/arguments.js
+++ js/arguments.js
@@ -1,10 +1,18 @@
-function addNumbers(num1){
-    let sum=0;
+function addNumbers(num1) {
+    let sum = 0;
     for (let i = 0; i < arguments.length; i++) {
         const num = arguments[i];
-        sum=sum+num;   
+        sum = sum + num;
     }
     return sum;
 }
-let result = addNumbers(4,5,6,25);
+let result = addNumbers(4, 5, 6, 25);
 console.log(result);
+
+//another example
+function getFullName(firstName, lastName) {
+    let fullName = firstName + lastName;
+    return fullName;
+}
+let myName=getFullName('Hasibul',' Hasan');
+console.log(myName);
