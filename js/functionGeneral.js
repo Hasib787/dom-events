4e1cbf829579f806179488af0ac76d848f0c0d9e
Hasib787 <mr.hasib45@gmail.com>
2021-01-31 16:10:43 +0600

    adding some function

---
+++ js/functionGeneral.js
@@ -0,0 +1,20 @@
+function addUser() {
+    let name = 'Hasib';
+    console.log(name);
+}
+addUser();
+// console.log(name);
+
+//function expression
+//  var name='Mofij Khan';
+var myTodo=function(){
+    console.log('Hello');
+};
+
+
+//IIFE (Immediately Invoked Function Expression
+(function(){
+    let favoriteColor='Blue';
+    console.log(favoriteColor);
+})();
+
